c55cb02640d1cc34429d1dec2631181d6f61baf4
IvanYFWANG1017 <83056310+IvanYFWANG1017@users.noreply.github.com>
2023-04-15 14:32:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Navigation scroll
-
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-      $('#navbar').removeClass('navbar-transparent');
-    } else {
-      $('#navbar').addClass('navbar-transparent');
-    }
-  });
-});
-
 // Courses accordion
 
 // Get DOM elements
@@ -31,38 +19,3 @@ buttons.forEach(button => {
     document.querySelector(`#${target}`).classList.add('active');
   });
 });
-
-
-
-// Contact form submission
-
-$('#contact-form').submit(function(e) {
-  e.preventDefault();
-  var name = $('#name').val();
-  var email = $('#email').val();
-  var message = $('#message').val();
-  $.ajax({
-    url: 'https://api.example.com/contact',
-    method: 'POST',
-    data: {
-      name: name,
-      email: email,
-      message: message
-    },
-    dataType: 'json',
-    success: function(data) {
-      $('#success-alert').fadeIn();
-      $('#name').val('');
-      $('#email').val('');
-      $('#message').val('');
-    },
-    error: function(xhr, status, error) {
-      $('#error-alert').fadeIn();
-    }
-  });
-});
-
-// Footer year
-
-var year = new Date().getFullYear();
-$('#footer-year').text(year);
