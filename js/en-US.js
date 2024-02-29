9695980621b499808dc45e9eb79fda542399ce60
IvanYFWANG1017 <83056310+IvanYFWANG1017@users.noreply.github.com>
2024-02-29 15:43:28 +0800

    Create en-US.js

---
+++ js/en-US.js
@@ -0,0 +1,9 @@
+JavaScript
+const lang = {
+  "home": "Home",
+  "about": "About Us",
+  "products": "Products and Services",
+  "contact": "Contact Us",
+  "language": "Language",
+  "switch": "Switch",
+};
